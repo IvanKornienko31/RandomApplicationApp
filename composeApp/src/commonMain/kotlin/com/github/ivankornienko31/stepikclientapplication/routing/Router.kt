42e7da31caf836efedc62a5c311162693dbe5c5a
IvanKornienko31 <i.k.kornienko@gmail.com>
2026-03-17 15:58:08 +0300

    Solved point 1 of homework

--- composeApp/src/commonMain/kotlin/com/github/ivankornienko31/stepikclientapplication/routing/Router.kt
+++ composeApp/src/commonMain/kotlin/com/github/ivankornienko31/stepikclientapplication/routing/Router.kt
@@ -1,16 +1,26 @@
 package com.github.ivankornienko31.stepikclientapplication.routing
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.toRoute
+import com.github.ivankornienko31.stepikclientapplication.datastore.OnboardingPreferences
 import com.github.ivankornienko31.stepikclientapplication.screens.greeting.presentation.GreetingScreen
 import com.github.ivankornienko31.stepikclientapplication.screens.login.presentation.LoginScreen
 import com.github.ivankornienko31.stepikclientapplication.screens.main.presentation.MainStepikScreen
+import com.github.ivankornienko31.stepikclientapplication.screens.main.presentation.StepikMainViewModel
 import com.github.ivankornienko31.stepikclientapplication.themes.StepikAppTheme
-import com.skydoves.compose.stability.runtime.TraceRecomposition
+import kotlinx.coroutines.launch
 
 /**
  * Функция [Router] отображает содержимое приложение с кастомной темой.
@@ -26,23 +36,46 @@ import com.skydoves.compose.stability.runtime.TraceRecomposition
  * @author Иван Корниенко
  */
 
-@TraceRecomposition
 @Composable
-@Preview
-fun Router() {
+fun Router(onboardingPreferences: OnboardingPreferences) {
+    val isFirstLaunch by onboardingPreferences.isFirstLaunch.collectAsStateWithLifecycle(null)
+    val coroutineScope = rememberCoroutineScope()
+
+    if (isFirstLaunch == null) {
+        Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+            CircularProgressIndicator()
+        }
+        return
+    }
+
     StepikAppTheme {
         val navController = rememberNavController()
+
+        val startDestination = if (isFirstLaunch == true) {
+            GreetingScreenRoute
+        } else {
+            LoginScreenRoute("login")
+        }
+
         NavHost(
             navController = navController,
-            startDestination = GreetingScreenRoute
+            startDestination = startDestination
         ) {
             composable<GreetingScreenRoute> {
                 GreetingScreen {
+                    coroutineScope.launch {
+                        onboardingPreferences.setFirstLaunchCompleted()
+                    }
+
                     navController.navigate(
                         LoginScreenRoute(
                             "login"
                         )
-                    )
+                    ) {
+                        popUpTo<GreetingScreenRoute> {
+                            inclusive = true
+                        }
+                    }
                 }
             }
             composable<LoginScreenRoute> { backStackEntry ->
